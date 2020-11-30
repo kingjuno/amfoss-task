bd6893aaffb552b773764d58ecb545263e0522d1
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2020-12-01 00:42:30 +0530

    Update sir_perceval_quest.py

--- task-08/sir_perceval_quest.py
+++ task-08/sir_perceval_quest.py
@@ -1,11 +1,20 @@
 import json
 import os
 import requests
-username = input("Enter the name:")
-request = requests.get('https://api.github.com/users/'+username+'/repos')
-json = request.json()
-for i in range(0,len(json)):
-  	print(json[i]['svn_url'],"")
-  	repo_url = json[i]['svn_url']
-  	os.system('perceval git --json-line '+repo_url+' >>commits.json')
+username = input()
+flag=1
+page=1
+name=[]
+while flag!=0:
+	request = requests.get('https://api.github.com/users/'+username+'/repos?page='+str(page))
+	page+=1
+	json = request.json()
+	if not (len(json)):
+		flag=0
+		continue
+	for i in range(0,len(json)):
+		name.append(json[i]['svn_url'])		
+print(len(name))	
+for i in range(0,len(name)):
+	os.system('perceval git --json-line '+name[i]+' >>commits.json')
 	
